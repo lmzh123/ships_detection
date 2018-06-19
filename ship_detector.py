a2c4f3b9aa4ad20d7c8df7cf1bca16d34ae0c977
Luis M. Zapata <luism.zapatah@gmail.com>
2018-06-19 13:37:09 -0500

    nms

--- ship_detector.py
+++ ship_detector.py
@@ -4,6 +4,7 @@ import argparse
 import time
 import cv2
 import numpy as np
+from nms import non_max_suppression_fast
 
 # HOG parametrization
 winSize = (32,32)
@@ -33,26 +34,20 @@ image = cv2.imread('scenes/lb_1.png')
 
 # loop over the image pyramid
 for resized in pyramid(image, scale=1.5):
+	print resized.shape
 	# loop over the sliding window for each layer of the pyramid
 	for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
 		# if the window does not meet our desired window size, ignore it
 		if window.shape[0] != winH or window.shape[1] != winW:
 			continue
 
-		# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
-		# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
-		# WINDOW
-		
-
-		# since we do not have a classifier, we'll just draw the window
 		clone = resized.copy()
 		cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
 		
 		cropped_img = resized[y:y + winH, x:x + winW]
 		cropped_img_resized = cv2.resize(cropped_img, winSize)
 		descriptor = np.transpose(hog.compute(cropped_img_resized))
-		prob = clf.predict_proba(descriptor)
-		y_pred = np.argmax(prob[0])
+		y_pred = clf.predict(descriptor)
 		
 		if y_pred == 1:
 			print 'Ship found!'
